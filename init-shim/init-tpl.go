3fcac1d1731f44ffb773dc7ed1f7b5f0a314b8e3
queer <null@amy.gg>
2022-09-03 03:26:45 -0600

    🚧 wip: printf debugging

--- init-shim/init-tpl.go
+++ init-shim/init-tpl.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"syscall"
 )
 
 func main() {
 	exe := "%EXE%"
 	finalArgs := []string{"%ARGS%"}
+	fmt.Println("init: exe=", exe, "args=", finalArgs)
 
 	syscall.Exec(exe, finalArgs, []string{})
 }
