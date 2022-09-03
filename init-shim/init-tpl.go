adcd5dd50f0cfcd8eed751b92d9d700bdadc8f16
queer <null@amy.gg>
2022-09-03 02:33:22 -0600

    🚧 wip: printf debugging

--- init-shim/init-tpl.go
+++ init-shim/init-tpl.go
@@ -6,14 +6,18 @@ import (
 )
 
 func main() {
-	fmt.Println("AAAAAAAAAAAAAAAA")
+	fmt.Println("starting init")
 	exe := "%EXE%"
 	finalArgs := []string{"%ARGS%"}
 
+	fmt.Println("cmd prep")
 	cmd := exec.Command(exe, finalArgs...)
+	fmt.Println("cmd prerun")
 	err := cmd.Run()
+	fmt.Println("cmd run")
 
 	if err != nil {
+		fmt.Println("kaboom")
 		fmt.Println(err.Error())
 	}
 }
