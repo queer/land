3813b4925dc25cef135efac123c9d49afbc53980
queer <null@amy.gg>
2022-09-03 04:36:17 -0600

    🐛 fix: Exec entrypoint always

--- init-shim/init-tpl.go
+++ init-shim/init-tpl.go
@@ -11,7 +11,10 @@ import (
 func main() {
 	finalArgs := []string{"%ARGS%"}
 	finalEnv := []string{"%ENV%"}
+	entrypoint := "%ENTRYPOINT%"
+	entrypointArgs := []string{"%ENTRYPOINT_ARGS%"}
 
+	// Set up initial path if provided
 	for _, arg := range finalEnv {
 		fmt.Println(arg)
 		if strings.HasPrefix(arg, "PATH=") {
@@ -22,16 +25,24 @@ func main() {
 		}
 	}
 
+	// Look up the exe since syscall.Exec doesn't
 	exe, err := exec.LookPath("%EXE%")
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
+	failFast(err)
 
 	fmt.Println("init: exe =", exe, "args =", finalArgs, "env =", finalEnv)
 
+	// Run entrypoint
+	err = exec.Command(entrypoint, entrypointArgs...).Run()
+	failFast(err)
+
+	// Run cmd
 	err = syscall.Exec(exe, finalArgs, finalEnv)
+	failFast(err)
+}
+
+func failFast(err error) {
 	if err != nil {
 		fmt.Println(err.Error())
+		os.Exit(1)
 	}
 }
